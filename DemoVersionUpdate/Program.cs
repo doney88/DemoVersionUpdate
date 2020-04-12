70fcaf5a265bcb4dc43f308364aba5d988891eb0
doney88 <cd_doney@hotmail.com>
2020-04-12 17:40:49 +0800

    添加项目文件。

---
+++ DemoVersionUpdate/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoVersionUpdate
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //检查版本是否与最新版本一致
+            string currentVersion="";
+            string serverVersion="";
+            string updateType="";
+
+            if (CheckSysVerionNew(ref currentVersion,ref serverVersion,ref updateType))
+            {
+                if (MessageBox.Show($"发现新版本！是否更新？ \r\n当前版本{currentVersion} \r\n最新版本{serverVersion}","版本更新",MessageBoxButtons.YesNo)== DialogResult.Yes)
+                {
+                    //异步打开下载程序
+                    Process.Start(Application.StartupPath + "\\Update\\Update.exe",updateType).Dispose();
+                    return;
+                }
+            }
+            Application.Run(new Form1());
+        }
+
+        private static bool CheckSysVerionNew(ref string currentVersion, ref string serverVersion,ref string updateType)
+        {
+            currentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            using (SqlConnection conn = new SqlConnection("server=.;database=HG;uid=sa;pwd=Chendong144216,"))
+            {
+                string sql = "SELECT TOP 1 FVersion,FUpdateType FROM tblSysVersion ORDER BY FUpdateTime DESC";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    serverVersion = dr["FVersion"].ToString();
+                    //int indexUpdateType = dr.GetOrdinal("FUpdateType");
+                    //updateType = dr.GetInt32(indexUpdateType);
+                    updateType = dr["FUpdateType"].ToString();
+
+                    if (serverVersion == null || serverVersion ==currentVersion)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Update/Update.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Update
+{
+    public partial class Update : Form
+    {
+        public Update()
+        {
+            InitializeComponent();
+        }
+
+        public Update(string updateType) : base()
+        {
+            MessageBox.Show(updateType);
+        }
+
+        private void btnCloseMainProcess_Click(object sender, EventArgs e)
+        {
+            CloseMainProcess();
+        }
+
+        private void CloseMainProcess()
+        {
+            System.Collections.ArrayList procList = new System.Collections.ArrayList();
+            foreach (System.Diagnostics.Process thisProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (thisProc.ProcessName == "DemoVersionUpdate")
+                { 
+                    if (!thisProc.CloseMainWindow())
+                        thisProc.Kill(); //当发送关闭窗口命令无效时强行结束进程                    
+                }
+            }
+        }
+
+        private void ReOpenMainProcess()
+        {
+            Process.Start(Application.StartupPath + "\\..\\DemoVersionUpdate.exe").Dispose();
+        }
+
+        private void btnReopenMainProcess_Click(object sender, EventArgs e)
+        {
+            ReOpenMainProcess();
+        }
+
+        private void btnUpdateMainProcess_Click(object sender, EventArgs e)
+        {
+            //经过Ftp将安装包下载至安装路径（如果有旧的注意覆盖）
+            //然后启动安装包覆盖安装
+            Process.Start(Application.StartupPath + "\\SetUp.exe").Dispose();
+        }
+    }
+}
